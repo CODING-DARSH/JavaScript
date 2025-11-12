7fdd7644d7a81324d37cab65e9a7924adef78d85
CODING-DARSH <darshvit20@gmail.com>
2025-11-12 19:00:26 +0530

    Updated tasks

--- PracticeFiles/tasks1.js
+++ PracticeFiles/tasks1.js
@@ -5,24 +5,22 @@ document.getElementById('update-button').onclick = function() {
 
     // Task 2: Change paragraph content using innerHTML
     document.querySelector('.data-text').innerHTML =
-        'This paragraph was updated using <strong>innerHTML</strong> with bold text!';
+        'Paragraph is updated';
 
     // Task 3: Get and display the innerText of a list item
     const listItemText = document.querySelector('.item').innerText;
     console.log('First list item text: ' + listItemText);
-
-    // Display it on the page
     const displayArea = document.querySelector('.output-area');
     displayArea.innerHTML += '<p>First list item: "' + listItemText + '"</p>';
 
     // Task 4: Add a new element using innerHTML
     const contentSection = document.getElementById('content-section');
-    contentSection.innerHTML += '<div>This is a new element added using innerHTML</div>';
-
+    contentSection.innerHTML += '<div>This is a new element</div>';
+    button.parentElement.innerHTML += '<p>Subtitle: ' + subtitleText + '</p>';
     // Task 5: Display the subtitle text below the button using innerText
     const subtitleText = document.getElementById('title-to-change').innerText;
     const button = document.getElementById('update-button');
-    button.insertAdjacentHTML('afterend', '<p>Subtitle: ' + subtitleText + '</p>');
+    button.parentElement.innerHTML += '<p>Subtitle: ' + subtitleText + '</p>';
 
     // Task 6: Replace the entire list using innerHTML
     const listContainer = document.getElementById('list-container');
@@ -34,7 +32,7 @@ document.getElementById('update-button').onclick = function() {
 
     // Task 7: Add a new <div> inside the content section using innerHTML
     contentSection.innerHTML +=
-        '<div><h3>Newly Added Div</h3><p>This div was added using innerHTML.</p></div>';
+        '<div><h3>Newly Added Div</h3></div>';
 
     // Task 8: Get a paragraph's text and display it using an alert box
     const paragraphText = document.querySelector('.data-text').textContent;
